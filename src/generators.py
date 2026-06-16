7125e4bbf446600876b0a3130213109ad26cc26b
witNie <witold.niec@gmail.com>
2026-06-16 08:35:16 +0200

    refactored OriginalGenerator

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,6 @@
 from random import choice
+from typing import Optional
+
 from src.validator import *
 from src.utils import encode_solution
 from copy import deepcopy
@@ -61,10 +63,14 @@ class RandomGenerator:
 
 
 class OrdinalGenerator:
-    def __init__(self, problem_instance: ProblemInstance):
+    def __init__(self, problem_instance: ProblemInstance, filename: Optional[str] = None):
         self.problem_instance = problem_instance
+        if filename:
+            self.filename = filename
+        else:
+            self.filename = None
 
-    def generate(self):
+    def generate_one(self):
         instance = deepcopy(self.problem_instance)
         subject_hours = deepcopy(instance.subject_hours)
         solution = []
@@ -126,27 +132,27 @@ class OrdinalGenerator:
         sol_sorted = sorted(sol, key=lambda x: (x.day, x.hour, x.classroom, x.teacher, x.subject))
         return encode_solution(sol_sorted)
 
+    def generate(self, want: int = 100, max_tries: int = 20000, save_to_file=False):
+        open(self.filename, "w").close()
+        seen: set[str] = set()
+        tries = 0
+        while len(seen) < want and tries < max_tries:
+            tries += 1
+            sol = self.generate_one()
+            if sol is None:
+                continue
 
-# Zostawione na potrzeby testowania generatora 2
-def main(problem: ProblemInstance, filename: str, want: int = 100, max_tries: int = 20000):
-    open(filename, "w").close()
-    seen: set[str] = set()
-    tries = 0
-    while len(seen) < want and tries < max_tries:
-        tries += 1
-        generator = OrdinalGenerator()
-        sol = generator.generate(problem)
-        if sol is None:
-            continue
+            key = self.canonical_key(sol)
+            if key in seen:
+                continue
 
-        key = generator.canonical_key(sol)
-        if key in seen:
-            continue
+            seen.add(key)
+            if save_to_file:
+                with open(self.filename, "a") as f:
+                    f.write(key + "\n")
 
-        seen.add(key)
-        with open(filename, "a") as f:
-            f.write(key + "\n")
+        print(f"Found {len(seen)} unique solutions in {tries} attempts.")
+        return list(seen)
 
-    print(f"Saved {len(seen)} unique solutions in {tries} attempts.")
 
 
